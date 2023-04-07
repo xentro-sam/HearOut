0768e20c31c90a3c9e525f93400a04193dd39422
Shivam Agarwal <itisshivam22@gmail.com>
2023-04-07 13:21:10 +0530

    fix: remove deprecated package

--- schemas.js
+++ schemas.js
@@ -1,5 +1,4 @@
 const BaseJoi = require('joi');
-const sanitizeHtml = require('sanitize-html');
 
 const extension = (joi) => ({
     type: 'string',
@@ -10,10 +9,7 @@ const extension = (joi) => ({
     rules: {
         escapeHTML: {
             validate(value, helpers) {
-                const clean = sanitizeHtml(value, {
-                    allowedTags: [],
-                    allowedAttributes: {},
-                });
+                const clean = value.replace(/&/g, '&amp;')
                 if (clean !== value) return helpers.error('string.escapeHTML', { value })
                 return clean;
             }
